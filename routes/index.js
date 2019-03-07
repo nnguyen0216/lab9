d7a66ae416adc9fbfab722484ca42b8a30c8142a
Nancy Nguyen <nnguyen0298@gmail.com>
2019-03-07 09:38:18 -0800

    lab9 complete

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+
+    
 /*
  * GET home page.
  */
@@ -5,12 +7,12 @@ var labs = require('../labs.json');
 
 exports.view = function(req, res){
   if(labs.length < 9) {
-  	var todaysLab = [{
+  	var todaysLab = {
 		"id": "lab-9",
 		"title": "Debugging 101",
 		"date": "March 7",
 		"image": "debug.png"
-	}];
+	};
 
   	labs.push(todaysLab);
   }
